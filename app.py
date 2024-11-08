215af77ae5480cd9f4dc03c2d95c99f7c476752d
RotichKipkoech <kipkoechrottich@gmail.com>
2024-11-08 11:12:46 +0300

    Completed Assingment

---
+++ app.py
@@ -0,0 +1,7 @@
+# Ask for user input
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+# Print a personalized message
+print(f"Hello, {name}! You are {age} years old and you're from {location}. Nice to meet you!")
